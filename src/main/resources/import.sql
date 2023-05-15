c31998e2b448681b9039d142a76ce968d1c06f78
michalHC <1554675+michalHC@users.noreply.github.com>
2023-05-15 04:20:29 +0200

    add db data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into tutorials values (1, 'Spring Data', 'Spring Data Description', true);
+insert into tutorials values (2, 'Java Spring Boot', 'Spring Framework Description', false);
+insert into tutorials values (3, 'Hibernate', 'Hibernate ORM Description', true);
+insert into tutorials values (4, 'Spring Boot', 'Spring Boot Description', false);
+insert into tutorials values (5, 'Spring JPA', 'Spring Data JPA Description', true);
+insert into tutorials values (6, 'Spring Batch', 'Spring Batch Description', true);
+insert into tutorials values (7, 'Spring Security', 'Spring Security Description', false);
